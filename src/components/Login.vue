3997eb0e4b0654f382f01c13dba70f75e4235ff8
邓闯 <1528275710@qq.com>
2021-08-12 22:23:55 +0800

    bug fix: 登录

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login_box">
+  <div class="login_container">
     登陆组件
   </div>
 
@@ -12,5 +12,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.login_container{
+  background-color: #2b4b6b;
+}
 </style>
