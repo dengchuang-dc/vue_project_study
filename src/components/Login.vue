f457efe7482ea3a94bdf1448f6ec91884fcb1c1a
邓闯 <1528275710@qq.com>
2021-08-12 23:44:11 +0800

    add data methods login box button

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,7 +4,20 @@
       <div class="avatar_box">
         <img src="../assets/logo.png" alt="">
       </div>
+      <el-form ref="loginFormRef" :model="login_form" :rules="rules" label-width="0px" class="login_form">
+        <el-form-item prop="username">
+          <el-input v-model="login_form.username" prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input prefix-icon="el-icon-lock"  v-model="login_form.password" type="password" ></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+
 
   </div>
 
@@ -12,26 +25,50 @@
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      login_form: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [
+          {required: true, message: '请输入登录名称', trigger: 'blur'},
+          {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
+        ],
+        password:[
+          {required: true, message: '请输入登录密码', trigger: 'blur'},
+          {min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur'}
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm(){
+      this.$refs.loginFormRef.resetFields()
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.login_container{
+.login_container {
   background-color: #2b4b6b;
   height: 100%;
 }
-.login_box{
-  width:450px;
+
+.login_box {
+  width: 450px;
   height: 300px;
   background-color: #ffffff;
   border-radius: 3px;
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
 
-  .avatar_box{
+  .avatar_box {
     height: 130px;
     width: 130px;
     border: 1px solid #eeeeee;
@@ -40,8 +77,9 @@ export default {
     box-shadow: 0 0 10px #ddd;
     position: absolute;
     left: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     background-color: #fff;
+
     img {
       width: 100%;
       height: 100%;
@@ -49,5 +87,18 @@ export default {
       background-color: #eeeeee;
     }
   }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/css/global.css'
+import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
